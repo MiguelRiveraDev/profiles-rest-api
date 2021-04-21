7628e88fe7f0ecf3620e291e9aa92a78b4d2b9b0
MiguelRiveraDev <miguelangel0207bcn@gmail.com>
2021-04-21 08:33:28 -0500

    6 Commit Added the API view

--- scripts/profilesAPI/views.py
+++ scripts/profilesAPI/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response 
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    # Test API View
+    def get(self, request, format = None):
+        # Returns a lis of APIView features
+        an_apiview = [
+            'Uses HTTP methods (get, post, parch, put, delete)',
+            'Traditional Django View',
+            'Gives you the most control over you app',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
